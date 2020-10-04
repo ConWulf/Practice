fa6dee23112a91c6e4998f48c32f485dc6dc530b
Connor Wulf <connor.m.wulf@gmail.com>
2020-10-04 17:32:44 -0500

    working on truthy function

---
+++ practice-solutions.js
@@ -0,0 +1,22 @@
+//Write a function that classifies the missing angle as either
+// "acute", "right", or "obtuse" based on its degrees.
+
+// function missingAngle(angle1, angle2) {
+//     if(180 - (angle1 + angle2) === 90){
+//         return "right";
+//     } else if(180 - (angle1 + angle2) > 90) {
+//         return "obtuse";
+//     } else {
+//         return "acute";
+//     }
+// }
+
+// function isTruthy(input) {
+//     if(input == false || input === null || input === undefined || NaN !== NaN){
+//         return 0;
+//     } else {
+//         return 1
+//     }
+// }
+//
+// console.log(isTruthy( NaN));
